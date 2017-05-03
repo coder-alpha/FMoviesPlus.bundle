359f5289f7e1954f1e5e6b55c06997066fa4b879
coder-alpha <coderalphaakv@gmail.com>
2017-05-03 15:43:41 -0400

    vid check fix test

--- Contents/Libraries/Shared/resources/lib/resolvers/host_gvideo.py
+++ Contents/Libraries/Shared/resources/lib/resolvers/host_gvideo.py
@@ -246,9 +246,9 @@ def check(url, videoData=None, headers=None, cookie=None, doPrint=False):
 						log('CHeck Fail', name, 'status == fail')
 						return (False, videoData)
 		else:
-			http_res, red_url = client.request(url=url, output='responsecodeext', followredirect=True, headers=headers, cookie=cookie)
+			http_res, red_url = client.request(url=url, output='responsecodeext', followredirect=True, headers=headers, cookie=cookie, httpsskip=True)
 			if http_res in client.HTTP_GOOD_RESP_CODES:
-				chunk = client.request(url=red_url, output='chunk', headers=headers, cookie=cookie) # dont use web-proxy when retrieving chunk
+				chunk = client.request(url=red_url, output='chunk', headers=headers, cookie=cookie, httpsskip=True) # dont use web-proxy when retrieving chunk
 				if doPrint:
 					print "url --- %s" % red_url
 					print "chunk --- %s" % chunk[0:20]

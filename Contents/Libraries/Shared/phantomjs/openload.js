88318b8a5957b71a117e6185ed304976493902af
coder-alpha <coderalphaakv@gmail.com>
2019-04-05 07:51:06 -0400

    fixed openload.js

--- Contents/Libraries/Shared/phantomjs/openload.js
+++ Contents/Libraries/Shared/phantomjs/openload.js
@@ -66,7 +66,6 @@ page.open('https://openload.co/embed/' + id + '/', function(status) {
 						if (oid != null) {
 							oid = oid.split('/');
 							oid = oid[oid.length-1];
-<<<<<<< HEAD
 						}
 					}
 					if (oid == null) {
@@ -83,24 +82,6 @@ page.open('https://openload.co/embed/' + id + '/', function(status) {
 							}
 						}
 					}
-=======
-						}
-					}
-					if (oid == null) {
-						return 'Exception: OID could not be found from window.';
-					}
-					var elms = document.getElementsByTagName(el);
-					for (var j = 0; j < elms.length; j++) {
-						if (elms[j].id.length > 0) {
-							var txt = elms[j].innerHTML;
-							if (txt.indexOf(oid+'~') > -1) {
-								return elms[j].id;
-							} else if (j == elms.length-1) {
-								return elms[j].id;
-							}
-						}
-					}
->>>>>>> origin/master
 					return elm;
 				} catch(e) {
 					return 'Exception: Error occurred.';
@@ -142,12 +123,7 @@ page.open('https://openload.co/embed/' + id + '/', function(status) {
 			var url = 'https://openload.co/stream/' + info.decoded_id + '?mime=true';
 			var url2 = 'https://openload.co/stream/' + info.decoded_id2 + '?mime=true';
 			var url_search = page.content.match(/\w+~\d+~[\d\.]+~\w+/);
-<<<<<<< HEAD
 			
-=======
-			var url_search2 = page.content.match(/\w+~\d+~[\d\.]+~\w+/);
-
->>>>>>> origin/master
 			if (info.decoded_id.indexOf(id+'~') > -1) {
 				//console.log('1');
 				console.log(url);
